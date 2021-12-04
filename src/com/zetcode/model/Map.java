26dd998c3d160c63f78ea14c5e817b6e822851b0
Your Name <you@example.com>
2021-12-04 08:10:40 +0700

    them phương thuc loadMap va setFacility

--- src/com/zetcode/model/Map.java
+++ src/com/zetcode/model/Map.java
@@ -1,11 +1,15 @@
 package com.zetcode.model;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.ParseException;
 
 import com.zetcode.controller.buttoncontroller.SaveButtonController;
 import com.zetcode.view.Board;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
 public class Map extends JSONArray {
 	String nameMap;
     public void SaveMap() {
@@ -21,7 +25,46 @@ public class Map extends JSONArray {
 		}
     }
     public void LoadMap(String nameFile) {
-      // TODO document why this method is empty
-    }  
+      Object obj;
+	  try{
+	  	obj = new JSONParser().parse(new FileReader(nameMap + ".json"));
+	  	JSONArray jsonArray = (JSONArray) obj;
+	  	Facility[] facilities = new Facility[30];
+	  	for (int i = 0; i < jsonArray.size(); i++) {
+		  	JSONObject jobj = (JSONObject)jsonArray.get(i);
+			String name = (String) jobj.get("name");
+			if (name == "Port") {
+				Port p = new Port(0,0);
+				setFacility(p, jobj);
+				facilities[i] = p;
+			}
+			if (name == "Lift") {
+				Lift l = new Lift(0,0);
+				setFacility(l, jobj);
+				facilities[i] = l;
+			}
+			if (name == "Room") {
+				Room r = new Room(0,0);
+				setFacility(r, jobj);
+				facilities[i] = r;
+			}
+	 	}
+      } catch (IOException e) {
+			e.printStackTrace();
+		} catch (org.json.simple.parser.ParseException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+		}
 }
+	private void setFacility(Facility f, JSONObject jobj) {
+		f.name = (String) jobj.get("name");
+		f.ID = (String) jobj.get("ID");
+		f.x = (int) jobj.get("x");
+		f.y = (int) jobj.get("y");
+		f.size_x = (int) jobj.get("size_x");
+		f.size_y = (int) jobj.get("size_y");
+
+	}
+}
+
 
